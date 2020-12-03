dee753d016ed7f283e26a46402ef951de2966fa7
LucasOJ <lucasodowdjones@gmail.com>
2020-12-03 12:05:10 +0000

    Started implementing reify

--- NbE.hs
+++ NbE.hs
@@ -9,36 +9,46 @@ data Expr = ExpVar Name
 
 type Name = String
 
--- Normal Form           
+-- Expressions with no reductions
 data NormalForm = NfNeutralForm NeutralForm
                 | NfLam Name NormalForm
 
+-- Expressions that can be reified (also normal forms)
 data NeutralForm = NeVar Name
                  | NeApp NeutralForm NormalForm
-
--- Environment
-type Env = Map Name V
 
 -- Semantics
 data V = Neutral NeutralForm 
        | Function (V -> V) 
+
+-- Environment
+type Env = Map Name V
 
 -- Converts syntax to semantics
 eval :: Expr -> Env -> V
 eval (ExpVar x)   env = lookupOrError x env
 eval (ExpLam x m) env = Function f where
         f :: V -> V
-        f y = eval m env' where
-            -- ISSUE: Overwrites cause problems for captured variables
-            -- (shouldn't be present anyway)
-            env' = insert x y env
+        f v = eval m env' where
+            env' = insert x v env
+eval (ExpApp m n) env = app (eval m env) (eval n env)
+        
+app :: V -> V -> V
+app (Function f) v = f v    
+app (Neutral n)  v = reflect (NeApp n (reify v))
 
-eval (ExpApp m n) env = app (eval m env) (eval n env) where
-        app :: V -> V -> V
-        app (Function f) v = f v    
-        -- ISSUE: Need normal form of v so depenent on reify
-        app (Neutral n)   v = Neutral (NeApp n (reify v))
-
--- TODO: Implement reify
 reify :: V -> NormalForm
-reify = undefined 
+reify (Neutral n)  = NfNeutralForm n
+-- ISSUE: Need fresh variables
+-- TODO: replace "x" with fresh variable
+reify (Function f) = NfLam "x" (reify (app (Function f) (reflect (NeVar "x"))))
+
+reflect :: NeutralForm -> V
+reflect = Neutral
+
+{- 
+    Why do we not need reflect in untyped version?
+        eval doing reflection for us?
+    Why do we not need reflect in intensional version?
+        extentional vs intentional?
+-}
