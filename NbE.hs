cebf434dd403709126aba604df474b69b22de327
LucasOJ <lucasodowdjones@gmail.com>
2021-02-04 14:50:00 +0000

    Fresh name state now a list of names

--- NbE.hs
+++ NbE.hs
@@ -3,11 +3,13 @@ import Data.Map ( empty,  insert, Map, mapKeys, lookup )
 import Utils ( lookupOrError )
 
 import Control.Monad.State ( MonadState(get), State, modify, evalState )
+import Data.Set ( Set, singleton, delete, union, notMember )
 
 type Name = Int
 
 -- State monad for generating fresh variable names
-type FreshName = State Name
+-- Fresh variables represented as a stream of variable names
+type FreshName = State [Name]
 
 -- Syntax
 data Expr = ExpVar Name
@@ -32,20 +34,32 @@ data V = Neutral NeutralForm
 -- Environment
 type Env = Map Name V
 
--- Converts syntax to semantics
+-- Evaluates the set of free variables given an expression
+freeVariables :: Expr -> Set Name
+freeVariables (ExpVar x) = singleton x
+freeVariables (ExpLam x m) = delete x (freeVariables m)
+freeVariables (ExpApp m n) = freeVariables m `union` freeVariables n
+
+-- Lazily produces a stream of fresh names for an expression given its free variables
+freshNameStream :: Set Name -> [Name]
+freshNameStream freeVariables = [x | x <- [0..], notMember x freeVariables]
+
+-- Core NbE
+
+-- Converts epxression syntax to semantics
 eval :: Expr -> Env -> FreshName V
 eval (ExpVar x) env = return v where
     v = case lookup x env of
         -- Bound variable, returns the semantic value bound to x in the environment
         Just y -> y
-        
-        -- Free variable
+
+        -- Free variable, embed into semantics as is
         Nothing -> Neutral (NeVar x)
-            
+
 eval (ExpLam var m) env = do
-    freshVar <- get
+    freshNames <- get
     -- Semantic function interpretation of the lambda expression syntax
-    let f v = evalState (eval m env') freshVar where
+    let f v = evalState (eval m env') freshNames where
             -- Defines a new environment where semantic input to function bound to var (for the body of the lambda)
             env' = insert var v env
     return (Function f)
@@ -69,9 +83,12 @@ app (Neutral n)  v = do
 reify :: V -> FreshName NormalForm
 reify (Neutral n)  = return (NfNeutralForm n)
 reify (Function f) = do
-    freshVar <- get
-    -- Since we introduced a new bound variable the state is not longer fresh so increment it
-    modify (+1)
+    freshNames <- get
+    -- Remove the first name from the freshNames stream
+    let freshVar = head freshNames
+    -- The first name is no longer fresh (we are abount to use it as a bound variable)
+    -- Modify the state to remove the used variable name
+    modify tail
     -- Reify the body of the semantic function when evaluated at the fresh bound variable
     normalForm <- reify (f (Neutral (NeVar freshVar)))
     return (NfLam freshVar normalForm)
@@ -79,15 +96,22 @@ reify (Function f) = do
 reflect :: NeutralForm -> V
 reflect = Neutral
 
+-- 'eval exp empty' evaluates the expression using the empty context into a semantic form shared by all beta-equivalent terms 
+-- 'reify' reifies the semantic form into its canonical normal form
+-- 'evalState' returns the normal form of exp at the initial state (the stream of initially fresh variables for exp) 
 normalise :: Expr -> Expr
-normalise exp = normalToExpr $ evalState (eval exp empty >>= reify) 0
+normalise exp = normalToExpr $ evalState (eval exp empty >>= reify) freshNames where
+    -- Generates the fresh name stream for exp
+    freshNames = (freshNameStream . freeVariables) exp
 
 --- Display
 
+-- Converts normal forms back to the expression syntax
 normalToExpr :: NormalForm -> Expr
 normalToExpr (NfNeutralForm n) = neutralToExp n
 normalToExpr (NfLam var n) = ExpLam var (normalToExpr n)
 
+-- Converts neutral forms back to the expression syntax
 neutralToExp :: NeutralForm -> Expr
 neutralToExp (NeVar i) = ExpVar i
 neutralToExp (NeApp n m) = ExpApp (neutralToExp n) (normalToExpr m)
