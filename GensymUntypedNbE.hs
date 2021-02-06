68e858fc466968c1acfaa5468aa6cf3d592ba627
LucasOJ <lucasodowdjones@gmail.com>
2021-02-06 20:00:22 +0000

    Removed unnecessary call to reify in Gensym by adding NeutralV data type

--- GensymUntypedNbE.hs
+++ GensymUntypedNbE.hs
@@ -1,6 +1,5 @@
 import Prelude hiding ( lookup, empty )
 import Data.Map ( empty,  insert, Map, mapKeys, lookup )
-import Utils ( lookupOrError )
 
 import Control.Monad.State ( MonadState(get), State, modify, evalState )
 import Data.Set ( Set, singleton, delete, union, notMember )
@@ -28,8 +27,11 @@ data NeutralForm = NeVar Name
     deriving (Read, Show)
 
 -- Semantics
-data V = Neutral NeutralForm
+data V = Neutral NeutralV
        | Function (V -> V)
+
+data NeutralV = NeVVar Int
+              | NeVApp NeutralV V
 
 -- Environment
 type Env = Map Name V
@@ -47,41 +49,37 @@ freshNameStream freeVariables = [x | x <- [0..], notMember x freeVariables]
 -- Core NbE
 
 -- Converts epxression syntax to semantics
-eval :: Expr -> Env -> FreshName V
-eval (ExpVar x) env = return v where
-    v = case lookup x env of
+eval :: Expr -> Env -> V
+eval (ExpVar x) env = case lookup x env of
         -- Bound variable, returns the semantic value bound to x in the environment
         Just y -> y
 
         -- Free variable, embed into semantics as is
-        Nothing -> Neutral (NeVar x)
+        Nothing -> Neutral (NeVVar x)
 
-eval (ExpLam var m) env = do
-    freshNames <- get
+eval (ExpLam var m) env = Function f where
     -- Semantic function interpretation of the lambda expression syntax
-    let f v = evalState (eval m env') freshNames where
-            -- Defines a new environment where semantic input to function bound to var (for the body of the lambda)
-            env' = insert var v env
-    return (Function f)
+    f v = eval m env' where
+        -- Defines a new environment where semantic input to function bound to var (for the body of the lambda)
+        env' = insert var v env
 
-eval (ExpApp m n) env = do
-    evalM <- eval m env
-    evalN <- eval n env
-    app evalM evalN
+eval (ExpApp m n) env = app (eval m env) (eval n env)
 
 -- Defines the application of semantic expressions
-app :: V -> V -> FreshName V
+app :: V -> V -> V
 -- Evaluates the semantic function f at v (ie evaluates redex)
-app (Function f) v = return (f v)
--- If the application does not reduce, create a semantic application
--- Need to reify v since NeApp :: NeutralForm -> NormalForm -> NeutralForm
-app (Neutral n)  v = do
-    reifiedV <- reify v
-    return (Neutral (NeApp n reifiedV))
+app (Function f) v = f v
+-- This justifies the used of NeutralV rather than NeutralForm
+-- NeApp constructor requires NormalForm rather than V
+-- Could use reify to transform V -> NormalForm but reify requires state so FreshName monad introduced to app
+-- Since eval calls app it would also require state
+app (Neutral n)  v = Neutral (NeVApp n v)
 
 -- Converts a sematic representation of a term into it's associated normal form
 reify :: V -> FreshName NormalForm
-reify (Neutral n)  = return (NfNeutralForm n)
+reify (Neutral n)  = do 
+    reifiedN <- reifyNeutral n
+    return (NfNeutralForm reifiedN)
 reify (Function f) = do
     freshNames <- get
     -- Remove the first name from the freshNames stream
@@ -90,17 +88,24 @@ reify (Function f) = do
     -- Modify the state to remove the used variable name
     modify tail
     -- Reify the body of the semantic function when evaluated at the fresh bound variable
-    normalForm <- reify (f (Neutral (NeVar freshVar)))
+    normalForm <- reify (f (Neutral (NeVVar freshVar)))
     return (NfLam freshVar normalForm)
 
-reflect :: NeutralForm -> V
-reflect = Neutral
+-- Converts a sematic representation of a neutral form into its associated semantic form
+reifyNeutral :: NeutralV -> FreshName NeutralForm
+reifyNeutral (NeVVar i)   = return (NeVar i)
+reifyNeutral (NeVApp n m) = do
+    reifiedNeutral <- reifyNeutral n
+    reifiedNormal  <- reify m
+    return (NeApp reifiedNeutral reifiedNormal) 
 
--- 'eval exp empty' evaluates the expression using the empty context into a semantic form shared by all beta-equivalent terms 
 -- 'reify' reifies the semantic form into its canonical normal form
 -- 'evalState' returns the normal form of exp at the initial state (the stream of initially fresh variables for exp) 
 normalise :: Expr -> Expr
-normalise exp = normalToExpr $ evalState (eval exp empty >>= reify) freshNames where
+normalise exp = normalToExpr $ evalState (reify semantics) freshNames where
+    -- evaluates the expression using the empty context into a semantic form shared by all beta-equivalent terms 
+    semantics = eval exp empty
+
     -- Generates the fresh name stream for exp
     freshNames = (freshNameStream . freeVariables) exp
 
