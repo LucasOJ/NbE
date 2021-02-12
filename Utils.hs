a39d67001094b6045068ee54c307dea9b27f3c3e
LucasOJ <lucasodowdjones@gmail.com>
2021-02-12 12:03:00 +0000

    Added comments and swapped argument orders

--- Utils.hs
+++ Utils.hs
@@ -1,4 +1,4 @@
-module Utils (  ) where
+module Utils ( getFreeVariables, getFreeVariableMapping, invertMap, getFreshVariableStream, exprToDeBruijnExpr, deBruijnExprToExpr ) where
 import Data.Set ( Set, singleton, delete, union, notMember, toList )
 import TypeDeclarations
 import qualified Data.Map as Map (Map, fromList, toList, lookup, insert, empty, mapKeys) 
@@ -10,52 +10,48 @@ getFreeVariables (ExpVar x) = singleton x
 getFreeVariables (ExpLam x m) = delete x (getFreeVariables m)
 getFreeVariables (ExpApp m n) = getFreeVariables m `union` getFreeVariables n
 
-freeVariableMapping :: [Name] -> Map.Map Name Int
-freeVariableMapping = Map.fromList . flip zip [0..]
+-- Given a set of free variables produces a map from free variable -> distinct int 
+-- Bijection between set of free variables and [0..#freeVariables]
+getFreeVariableMapping :: Set Name -> Map.Map Name Int
+getFreeVariableMapping = Map.fromList . flip zip [0..] . toList
 
+-- Swaps the keys and values of a Map
+-- Given a bijection, finds the inverse
 invertMap :: (Ord a, Ord b) => Map.Map a b -> Map.Map b a
 invertMap = Map.fromList . map swap . Map.toList
 
+-- Produces a stream of fresh variables 
+-- All variable names in the stream don't collide with any free variables in the term to prevent variable capture
 getFreshVariableStream :: Set Name -> [Name]
 getFreshVariableStream freeVars = [freshVariable i | i <- [0..], notMember (freshVariable i) freeVars] where
-        freshVariable i = "x" ++ show i
+        freshVariable i = "v" ++ show i
 
-exprToDeBruijnExpr :: Expr -> Map.Map Name Int -> DbExpr
-exprToDeBruijnExpr (ExpVar x) context = maybe undefined DbVar (Map.lookup x context)
-exprToDeBruijnExpr (ExpLam x m) context = DbLam (exprToDeBruijnExpr m context') where
+-- Given the mapping of free variables to deBruijn indexes, produce the deBruijn respresentation of a term
+exprToDeBruijnExpr :: Map.Map Name Int -> Expr -> DbExpr
+-- Technically this function could return undefined
+-- Correctness guarentees that lookups are on peformed on names in the context
+exprToDeBruijnExpr context (ExpVar x)   = maybe undefined DbVar (Map.lookup x context)
+exprToDeBruijnExpr context (ExpLam x m) = DbLam (exprToDeBruijnExpr context' m) where
+        -- Since we enter the body of a new lambda, the named variable maps to the 0th abstraction index in context'
+        -- The rest of the mapped indicies are incremented by 1
         context' = Map.insert x 0 (fmap (+1) context)
-exprToDeBruijnExpr (ExpApp m n) context = DbApp (exprToDeBruijnExpr m context) (exprToDeBruijnExpr n context)
+exprToDeBruijnExpr context (ExpApp m n) = DbApp (exprToDeBruijnExpr context m) (exprToDeBruijnExpr context n)
 
-deBruijnExprToExpr :: DbExpr -> Map.Map Int Name -> [Name] -> Expr 
-deBruijnExprToExpr (DbVar x) context _              = maybe undefined ExpVar (Map.lookup x context)
-deBruijnExprToExpr (DbLam m) context (freshVar:freshVarStream') = 
-        ExpLam freshVar (deBruijnExprToExpr m context' freshVarStream') where
+-- Given the mapping of deBruijn indexes to freeVariables, produce the string-named representation of a term
+deBruijnExprToExpr :: Map.Map Int Name -> [Name] -> DbExpr -> Expr
+-- Technically this function could return undefined
+-- Correctness guarentees that lookups are on peformed on indicies in the context
+deBruijnExprToExpr context _                          (DbVar x)   = maybe undefined ExpVar (Map.lookup x context)
+deBruijnExprToExpr context (freshVar:freshVarStream') (DbLam m)   = 
+        ExpLam freshVar (deBruijnExprToExpr context' freshVarStream' m) where
+        -- Performs the same transformation as the exprToDeBruijnExpr lambda case
+        -- However in this case the keys and values have swapped since we are considering the bijection in the other direction
         context' = Map.insert 0 freshVar (Map.mapKeys (+1) context)
-deBruijnExprToExpr (DbApp m n) context freshVarStream = 
-        ExpApp (deBruijnExprToExpr m context freshVarStream) (deBruijnExprToExpr n context freshVarStream)
+-- It is ok to "split" freshVarStream here as the bound variables of each subterm are indepdendent 
+deBruijnExprToExpr context freshVarStream             (DbApp m n) = 
+        ExpApp (deBruijnExprToExpr context freshVarStream m) (deBruijnExprToExpr context freshVarStream n)
 
 -- Debug
 
 expr :: Expr
 expr = ExpApp (ExpLam "x" (ExpApp (ExpVar "x") (ExpVar "x0"))) (ExpLam "y" (ExpVar "x1"))
-
-expr_fv :: Set Name
-expr_fv = getFreeVariables expr
-
-expr_fv_list :: [Name]
-expr_fv_list = toList expr_fv
-
-fv_map :: Map.Map Name Int
-fv_map = freeVariableMapping expr_fv_list
-
-dBExpr :: DbExpr 
-dBExpr = exprToDeBruijnExpr expr fv_map
-
-reverseFVMap :: Map.Map Int Name
-reverseFVMap = invertMap fv_map
-
-fresh_vars :: [Name]
-fresh_vars = getFreshVariableStream expr_fv
-
-expr' :: Expr 
-expr' = deBruijnExprToExpr dBExpr reverseFVMap fresh_vars
